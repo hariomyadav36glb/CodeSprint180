4b5b86ab8a0a84e5931f479e3fb2d03ba991dc6b
hariom yadav <153520176+hariomyadav36glb@users.noreply.github.com>
2025-02-18 00:47:40 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -285,37 +285,37 @@ vector<int>&t)
 // Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
 
 
-class Solution {
-public:
-    string removeKdigits(string num, int k) {
-      stack<char>st;
-      int n = num.size();
-      for(int i=0;i<n;i++)
-      {
-        while(!st.empty() && k>0 && num[i]-'0' < st.top() - '0')
-        {
-            st.pop();
-            k--;
-        }
-        st.push(num[i]);
-      } 
-      while(k-- && !st.empty())
-      {
-        st.pop();
-      } 
-      if(st.empty()) return "0";
-      string res="";
-      while(!st.empty())
-      {
-        res+=st.top();
-        st.pop();
-      }
-      while(res.size() && res.back()=='0')
-      {
-        res.pop_back();
-      }
-      reverse(res.begin(),res.end());
-      if(res.empty()) return "0";
-      return res;
-    }
-};
+// class Solution {
+// public:
+//     string removeKdigits(string num, int k) {
+//       stack<char>st;
+//       int n = num.size();
+//       for(int i=0;i<n;i++)
+//       {
+//         while(!st.empty() && k>0 && num[i]-'0' < st.top() - '0')
+//         {
+//             st.pop();
+//             k--;
+//         }
+//         st.push(num[i]);
+//       } 
+//       while(k-- && !st.empty())
+//       {
+//         st.pop();
+//       } 
+//       if(st.empty()) return "0";
+//       string res="";
+//       while(!st.empty())
+//       {
+//         res+=st.top();
+//         st.pop();
+//       }
+//       while(res.size() && res.back()=='0')
+//       {
+//         res.pop_back();
+//       }
+//       reverse(res.begin(),res.end());
+//       if(res.empty()) return "0";
+//       return res;
+//     }
+// };
